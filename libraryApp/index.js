2cfe5ad6a1a9a040f808362f726d806e29eab337
Rajat Sharma <rajat08sh@gmail.com>
2023-07-31 17:50:33 +0530

    Update index.js

--- libraryApp/index.js
+++ libraryApp/index.js
@@ -21,6 +21,7 @@ MongoClient.connect(dburl, function (err, db) {
 });
 
 app.use(bodyParser.json());
+app.use('/', routes);
 
 const port = 3000;
 app.listen(port, () => { console.log(`Server is running on port ${port}`) });
